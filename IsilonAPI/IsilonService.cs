f13c76d6f27b9a44e28e2715d2124aa9a19eb02e
Scapes23 <jeff@jeffgrubb.org>
2015-02-04 17:27:07 -0500

    Added Service

---
+++ IsilonAPI/IsilonService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace IsilonAPI
+{
+    public class IsilonService
+    {
+        public string Username;
+        public string Password;
+        public string IsilonUrl;
+        public bool IgnoreInvalidCerts;
+
+        // public string AuthToken -- todo, Basic/Token authentication
+
+        public IsilonService(string username, string password, string isilonUrl, bool ignoreInvalidCerts)
+        {
+            Username = username;
+            Password = password;
+            IsilonUrl = isilonUrl;
+            IgnoreInvalidCerts = ignoreInvalidCerts;
+        }
+
+        public HttpWebRequest CreateRequest(string resource)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(IsilonUrl + resource);
+
+            request.Headers.Add("Authorization", "Basic " + Convert.ToBase64String(new ASCIIEncoding().GetBytes(Username + ":" + Password)));
+
+            return request;
+
+        }
+    }
+}
