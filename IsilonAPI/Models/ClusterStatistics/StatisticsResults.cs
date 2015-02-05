16ce5c8a8279a75289c010cb9dfe642bf8f74947
Scapes23 <jeff@jeffgrubb.org>
2015-02-05 18:10:13 -0500

    Added Cluster Statistics

---
+++ IsilonAPI/Models/ClusterStatistics/StatisticsResults.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace IsilonAPI.Models.ClusterStatistics
+{
+    [DataContract]
+    public class StatisticsResults
+    {
+        [DataMember(Name = "keys")]
+        public StatisticsKeys[] Statistics;
+    }
+}
